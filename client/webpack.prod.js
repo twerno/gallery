08d214697d2b71f62b5ea1ff095b06adfda41603
twerno <twerno@gmail.com>
2020-02-13 22:08:38 +0100

    heroku deploy

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,15 +1,10 @@
-const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
     mode: 'production',
-    output: {
-        filename: 'app.js',
-        chunkFilename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'public/js'),
-    },
+
     optimization: {
         minimize: true,
     },
--- deploy_2_heroku.bat
+++ deploy_2_heroku.bat
@@ -1,8 +1,10 @@
 cd client
+del /S /Q dist
 call npm run build-prod
 cd ..
 del /S /Q server\static
 xcopy client\public server\static /s
+xcopy client\dist server\static /s
 call git add .
 call git commit -am "heroku deploy"
 call git subtree push --prefix server heroku master
